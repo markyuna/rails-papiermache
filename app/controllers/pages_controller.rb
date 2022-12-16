b395d89de2e54e7a83884061f31fefc71c38d338
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2022-12-16 13:41:09 +0100

    rails-papiermache

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+# app/controllers/pages_controller.rb
+class PagesController < ApplicationController
+  def index
+  end
+
+  def apropos
+  end
+
+  def sculptures
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# config/routes.rb
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#index'
+
+  get 'apropos', to: 'pages#apropos', as: :apropos
+  get 'sculptures', to: 'pages#sculptures', as: :sculptures
+  get 'contact', to: 'pages#contact', as: :contact
 end
